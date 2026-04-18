c78f1aa00f852b4908de64efb9327b60a7e65d22
Jakub Olszówka <j.olszowka@op.pl>
2026-04-18 13:02:59 +0200

    Kontynacja rozwoju UI

--- app/src/main/java/pl/edu/anstar/flavorforge/MainActivity.kt
+++ app/src/main/java/pl/edu/anstar/flavorforge/MainActivity.kt
@@ -1,20 +1,63 @@
+//package pl.edu.anstar.flavorforge
+//
+//import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+
+
+//co5 s
 package pl.edu.anstar.flavorforge
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val layouts = listOf(
+        R.layout.activity_main,
+        R.layout.activity_sign_in,
+        R.layout.activity_sign_up,
+        R.layout.drawer_menu,
+
+    )
+
+    private var index = 0
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        startLoop()
+    }
+
+    private fun startLoop() {
+        handler.post(object : Runnable {
+            override fun run() {
+                setContentView(layouts[index])
+
+                index = (index + 1) % layouts.size
+
+                handler.postDelayed(this, 5000) // 2 sekundy
+            }
+        })
     }
 }
